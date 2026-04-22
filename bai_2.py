f8256d11338f7445ae976f1ca84c07a9a7bb92a8
benhv1en <184822003+benhv1en@users.noreply.github.com>
2026-04-22 14:41:00 +0700

    them bai 2
    them bai 2

--- bai_2.py
+++ bai_2.py
@@ -1,2 +1,5 @@
-def Standardise(list_item) -> list[str]:
-    
+def Standardise(list_item: list[str]) -> list[str]:
+    for item in list_item:
+        while " " in item:
+            item.replace(" ", "")
+        item.capitalize()

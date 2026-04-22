a614a9aa8856c94ca346ee1121cd74e4244e5819
benhv1en <184822003+benhv1en@users.noreply.github.com>
2026-04-22 15:29:32 +0700

    ditmemay
    ditmemay

--- bai_5.py
+++ bai_5.py
@@ -1,1 +1,19 @@
+import matplotlib.pyplot as plt
+
 my_dict = {"Thang 1": 45.5, "Thang 2": 112.0, "Thang 3": 89.5}
+sum = 0
+for month in my_dict.values():
+    sum = sum + month
+
+min_month = min(my_dict, key=my_dict.get)
+print("Thang co luong mua it nhat la %s" % min_month)
+print("Tong luong mua = %f" % sum)
+
+x = list(my_dict.keys())
+y = list(my_dict.values())
+
+plt.plot(x, y)
+plt.xlabel("Thang")
+plt.ylabel("Luong mua")
+plt.title("Luong mua trong quy 1")
+plt.show()

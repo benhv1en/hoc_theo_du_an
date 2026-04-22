18f2efee10f2543cfeddf4b226c56b3d3adf79ba
benhv1en <184822003+benhv1en@users.noreply.github.com>
2026-04-22 15:06:19 +0700

    sua lai bai 2
    sua lai bai 2

--- bai_2.py
+++ bai_2.py
@@ -2,13 +2,12 @@ def Standardise(list_item: list[str]) -> list[str]:
     for i in range(0, len(list_item)):
         while " " in list_item[i]:
             list_item[i] = list_item[i].replace(" ", "")
-        for letter in list_item[i]:
-            letter = letter.capitalize()
+        list_item[i] = list_item[i].upper()
     list_item.sort()
     return list_item
 
 
 if "__main__" == __name__:
-    li = ["sp-001", "SP-002", "sP-003"]
+    li = ["sp-002", "SP-001", "sP-003"]
     li = Standardise(li)
     print(li)
---
+++ bai_5.py
@@ -0,0 +1,1 @@
+my_dict = {"Thang 1": 45.5, "Thang 2": 112.0, "Thang 3": 89.5}
